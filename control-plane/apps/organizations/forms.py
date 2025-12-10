106f3de9f3b9326a5f1518f8540bfc7d46c85f18
fidiego <sayheytofred@gmail.com>
2025-12-10 16:51:05 -0600

    chore: disable us-west-1

--- control-plane/apps/organizations/forms.py
+++ control-plane/apps/organizations/forms.py
@@ -3,12 +3,26 @@ from .models import Organization
 
 
 class OrganizationForm(forms.ModelForm):
+    DISABLED_REGIONS = [
+        Organization.REGION_US_WEST_1,
+    ]
+
     class Meta:
         model = Organization
         fields = ["name", "region", "deploy_headlamp", "deploy_tailscale"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["region"].choices = [
+            choice
+            for choice in Organization.REGION_CHOICES
+            if choice[0] not in self.DISABLED_REGIONS
+        ]
+
     def clean_name(self):
         name = self.cleaned_data.get("name")
         if Organization.objects.filter(name=name).exists():
-            raise forms.ValidationError("An organization with this name already exists.")
+            raise forms.ValidationError(
+                "An organization with this name already exists."
+            )
         return name
